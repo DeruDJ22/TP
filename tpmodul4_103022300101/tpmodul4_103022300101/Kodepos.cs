e54d151be0827c9758a35a705b6fd610e7ecb97e
Kuroakai <derudj12345@gmail.com>
2025-03-09 21:03:31 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul4_103022300101/tpmodul4_103022300101/Kodepos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static tpmodul4_103022300101.Kodepos;
+
+namespace tpmodul4_103022300101
+{
+    class Kodepos
+    {
+        public enum kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja };
+
+        // Metode untuk mendapatkan kode pos berdasarkan kelurahan
+        public static int GetKodePos(kelurahan Kelurahan)
+        {
+            int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+            return kodePos[(int)Kelurahan];
+        }
+    }
+}
